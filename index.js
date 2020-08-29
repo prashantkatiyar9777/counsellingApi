1c472aa6e32d0c2e8bbb4c46a72df4798649995b
Aditya <cs19b1003@iiitr.ac.in>
2020-08-29 13:42:53 +0530

    fix category filter.

--- index.js
+++ index.js
@@ -27,29 +27,23 @@ app.get("/v1/institute-data/:institute", (req, res)=>{
 })
 
 app.get("/v1/institute-with-rank", (req, res) =>{
-    const rank = req.query.rank;
+    const rank = Number(req.query.rank);
     const category = req.query.category;
     const opening = category+".data.opening"
     const closing = category+".data.closing"
-    var query = {
-        $and : [{
-            [opening]: {
-                $lt : rank
-            }
-        },{
-            [closing]: {
-                $gt : rank
-            }
-        }
-    ]
-    }
 
-    //TODO: Category wise filter
-    Database.collection("2019").find({"gen.data.opening": {$lt: rank}, "gen.data.closing": {$gt: rank}}, {projection: {name: 1, [category]:1}}).toArray((err, data)=>{
+    var query = {
+        [closing]: {
+            $lt: rank
+        }
+    }
+    Database.collection("2019").find(query, {projection: {name: 1, [category]:1}}).toArray((err, data)=>{
         if(err) return res.statusCode(500);
         res.json(data);
         console.log(data.length);
     })
+
+    //TODO : Filter out data
 }); 
 
 
