76dba3066de2244021af4f733c6b0088f74902fc
Aditya <cs19b1003@iiitr.ac.in>
2020-08-29 14:48:20 +0530

    fix db

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var Database;
 
 
 // Variables to be converted to use enviornment variables
-process.env.dbUrl = "mongodb://localhost:27017/counselling";
+// process.env.dbUrl = "mongodb://localhost:27017/counselling";
 
 
 //Database
@@ -32,26 +32,7 @@ app.get("/v1/institute-data/:institute", (req, res)=>{
 })
 
 app.get("/v1/institute-with-rank", (req, res) =>{
-
     instituteWithRank(req, res, Database);
-
-    // const rank = Number(req.query.rank);
-    // const category = req.query.category;
-    // const opening = category+".data.opening"
-    // const closing = category+".data.closing"
-
-    // var query = {
-    //     [closing]: {
-    //         $gt: rank
-    //     }
-    // }
-    // Database.collection("2019").find(query, {projection: {name: 1, [category]:1}}).toArray((err, data)=>{
-    //     if(err) return res.statusCode(500);
-    //     res.json(data);
-    //     console.log(data.length);
-    // })
-
-    // //TODO : Filter out data
 }); 
 
 
